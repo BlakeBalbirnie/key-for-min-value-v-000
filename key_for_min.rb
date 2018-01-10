90835551fff252040edb31482f174eabd2a477d6
Blake Balbirnie <blakebalbirnie@gmail.com>
2018-01-10 15:50:55 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  birthday_kids = {
+      "Timmy" => 9,
+      "Sarah" => 6,
+      "Amanda" => 27
+  }
 
+  birthday_kids.collect do |kids_name, age|
+      age.min
+  end
 end
